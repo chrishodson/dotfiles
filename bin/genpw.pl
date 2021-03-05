9738a3d6bf25f0790a2af61b490d82d6328263a8
Christopher Hodson <5891340+chrishodson@users.noreply.github.com>
2021-03-05 13:49:44 -0500

    Initial commit

---
+++ bin/genpw.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+# vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
+
+my $length = 8;
+if (@ARGV) {
+  $length = $ARGV[0];
+}
+
+print generatePassword($length) . "\n";
+exit;
+
+
+sub generatePassword {
+  my $password;
+  my $length = shift;
+  my $nice = 'abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+  my $nums = '23456789';
+  my $possible = $nice . '!@#%&<>/-=+';
+  #Double the chances of getting "nice" characters
+  $possible = $possible . $nums . $nice ;
+  $previous = ' ';
+  $newletter = ' ';
+  while (length($password) < $length) {
+    until ($newletter != $previous) {
+      $newletter = substr($possible, (int(rand(length($possible)))), 1);
+    }
+    $password .= $newletter;
+    $previous = $newletter;
+  }
+  return $password;
+} 
